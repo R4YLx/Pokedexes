465c4bcd5b136cca204f6c811f98dc9894a881a5
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-24 15:40:45 +0100

    setting overflow-y-scroll and hiding scrollbar in Display

--- react-pokedex/src/components/Pokedex/Display/Display.tsx
+++ react-pokedex/src/components/Pokedex/Display/Display.tsx
@@ -5,7 +5,7 @@ const Display = ({ children, className }: DisplayProps) => {
     <div
       className={`bg-slate-300 border border-black min-h-full rounded-bl-3xl rounded-tr-3xl flex ${className}`}
     >
-      <div className="bg-green-600 border border-black m-6 basis-full">
+      <div className="bg-green-600 border border-black m-6 basis-full overflow-y-scroll scrollbar-hide">
         {children}
       </div>
     </div>
