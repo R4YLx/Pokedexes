876a997c2c6b3c4cf3e6361fc370aa67721072e2
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-25 20:30:26 +0100

    setting up project

---
+++ svelte-pokedex/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
+			},
+			colors: {
+				'pokedex-dark-red': '#AD0000',
+				'pokedex-red': '#CC0000',
+				'pokedex-blue': '#0076CA',
+				'pokedex-light-blue': '#36B5FF'
+			},
+			fontFamily: {
+				display: ['"Press Start 2P"', 'cursive']
+			}
+		}
+	},
+	plugins: [require('tailwind-scrollbar-hide')]
+}
