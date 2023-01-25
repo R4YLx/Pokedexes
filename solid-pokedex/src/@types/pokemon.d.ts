31b22aa245ef2f4d93e96605baeddc7d45775a56
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-25 19:17:13 +0100

    setting up project

---
+++ solid-pokedex/src/@types/pokemon.d.ts
@@ -0,0 +1,38 @@
+interface IPokemon {
+  id?: number
+  name: string
+  height?: number
+  order?: number
+  weight?: number
+  moves?: PokemonMove[]
+  sprites?: {
+    front_default: string
+  }
+  stats?: PokemonStat[]
+  types?: PokemonType[]
+  url?: string
+}
+
+interface PokemonMove {
+  move: {
+    name: string
+    url: string
+  }
+}
+
+interface PokemonStat {
+  base_stat: number
+  effort: number
+  stat: {
+    name: string
+    url: string
+  }
+}
+
+interface PokemonType {
+  slot: number
+  type: {
+    name: string
+    url: string
+  }
+}
