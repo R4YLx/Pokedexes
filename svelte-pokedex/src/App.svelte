8acc0ecdfe36da5be14e759ad0eb10c34d6acbb9
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-27 21:15:33 +0100

    fixing tailwind for svelte and composing button

--- svelte-pokedex/src/App.svelte
+++ svelte-pokedex/src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-
+import Button from '@components/Button/Button.svelte'
 </script>
 
 <main>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Button
+    intent="rounded"
+    bgColor="grey"
+    size="md"
+    on:click="{() => alert('asfafafaf')}" />
 </main>
-
-<style>
- 
-</style>
---
+++ svelte-pokedex/src/components/Button/Button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { cva, type VariantProps } from 'cva'
+
+const buttonStyles = cva(
+  'p-2 drop-shadow-lg border border-black flex justify-center items-center',
+  {
+    variants: {
+      intent: {
+        rounded: ['rounded-full'],
+        squared: ['rounded-md']
+      },
+      bgColor: {
+        grey: ['bg-gray-400'],
+        yellow: ['bg-yellow-400'],
+        blue: ['bg-pokedex-light-blue']
+      },
+      size: {
+        sm: ['min-h-14 min-w-14'],
+        md: ['h-16 w-16']
+      }
+    },
+    defaultVariants: {
+      intent: 'squared',
+      bgColor: 'grey',
+      size: 'sm'
+    }
+  }
+)
+
+interface ButtonProps extends VariantProps<typeof buttonStyles> {}
+
+export let intent: ButtonProps['intent']
+export let bgColor: ButtonProps['bgColor']
+export let size: ButtonProps['size']
+</script>
+
+<button on:click class="{buttonStyles({ intent, bgColor, size })}"
+  ><slot /></button>
